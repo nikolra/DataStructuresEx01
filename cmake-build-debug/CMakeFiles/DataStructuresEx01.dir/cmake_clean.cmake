cfb14d0255ace978e34f89ee6983b9c99fa49b1d
nikolra <nikolra@mta.ac.il>
2021-11-14 15:45:58 +0200

    Initial commit

---
+++ cmake-build-debug/CMakeFiles/DataStructuresEx01.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/DataStructuresEx01.dir/main.cpp.o"
+  "DataStructuresEx01"
+  "DataStructuresEx01.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/DataStructuresEx01.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/DataStructuresEx01.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for DataStructuresEx01.
+# This may be replaced when dependencies are built.
